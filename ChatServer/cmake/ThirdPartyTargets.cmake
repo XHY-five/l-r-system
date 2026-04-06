6ed1763bf6ca24af8364bcfeeb6d86d2c843c328
xhy <104206943+XHY-five@users.noreply.github.com>
2026-04-06 14:20:28 +0800

    Delete ChatServer directory

--- ChatServer/cmake/ThirdPartyTargets.cmake
+++
@@ -1,9 +0,0 @@
-include_guard(GLOBAL)
-
-if(WIN32)
-  include("${CMAKE_CURRENT_LIST_DIR}/ThirdPartyTargetsWindows.cmake")
-elseif(UNIX AND NOT APPLE)
-  include("${CMAKE_CURRENT_LIST_DIR}/ThirdPartyTargetsLinux.cmake")
-else()
-  message(FATAL_ERROR "Unsupported platform for ChatServer: ${CMAKE_SYSTEM_NAME}")
-endif()
